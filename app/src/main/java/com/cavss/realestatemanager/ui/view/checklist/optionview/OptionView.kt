b3e14647a1dd3d1bc8cb7fddcea5f1138d8be608
JuYeon Lee <cavss1118@gmail.com>
2021-12-03 23:45:03 +0900

    #7 체크리스트 옵션뷰 // 1. chip 클릭시 리스트를 filter해서 재구성

---
+++ app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/optionview/OptionView.kt
@@ -0,0 +1,58 @@
+package com.cavss.realestatemanager.ui.view.checklist.optionview
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cavss.realestatemanager.R
+
+@Composable
+fun OptionView(){
+    val context = LocalContext.current
+    LazyRow(
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth(),
+        content ={
+            items(context.resources.getStringArray(R.array.rent_type_list)){ item ->
+                Text(
+                    text = item,
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .padding(
+                            vertical = 5.dp,
+                            horizontal = 10.dp
+                        )
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(colorResource(id = R.color.opacityGreen))
+                        .border(
+                            1.dp,
+                            Color.White,
+                            RoundedCornerShape(10.dp)
+                        )
+                        .padding(
+                            vertical = 5.dp,
+                            horizontal = 10.dp
+                        )
+                        .clickable {
+
+                        },
+                )
+            }
+        })
+}
