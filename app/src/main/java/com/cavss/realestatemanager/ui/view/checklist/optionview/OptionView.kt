2b2fb8279b9524c4ebc1f27eb0370e8e43b50270
JuYeon Lee <cavss1118@gmail.com>
2021-12-04 22:11:33 +0900

    #8 OptionView // 1. chip 클릭시, 해당 리스트를 chip의 맞게 변경

--- app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/optionview/OptionView.kt
+++ app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/optionview/OptionView.kt
@@ -1,10 +1,12 @@
 package com.cavss.realestatemanager.ui.view.checklist.optionview
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -14,25 +16,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cavss.realestatemanager.R
 
 @Composable
-fun OptionView(){
-    val context = LocalContext.current
+fun OptionView(
+    height : Dp
+){
+
     LazyRow(
         horizontalArrangement = Arrangement.Center,
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .height(height),
         content ={
-            items(context.resources.getStringArray(R.array.rent_type_list)){ item ->
+            items(OptionModel.OPTION_LIST.list){ item ->
                 Text(
-                    text = item,
+                    text = stringResource(id = item.inNative),
                     color = Color.White,
-                    fontSize = 20.sp,
+                    fontSize = 18.sp,
                     modifier = Modifier
                         .padding(
                             vertical = 5.dp,
