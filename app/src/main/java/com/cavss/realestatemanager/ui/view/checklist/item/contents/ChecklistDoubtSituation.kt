ac3a666fccf574e0ebaaef89644d5feb17292f30
JuYeon Lee <cavss1118@gmail.com>
2021-12-04 22:30:02 +0900

    #13 ChecklistDoubtSituation // 1. container를 만들어서, 이모지 / 타이틀 / 콘텐트로 나누기

--- app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/item/contents/ChecklistDoubtSituation.kt
+++ app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/item/contents/ChecklistDoubtSituation.kt
@@ -1,39 +1,52 @@
 package com.cavss.realestatemanager.ui.view.checklist.item.contents
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.cavss.realestatemanager.R
 
 @Composable
 fun ChecklistDoubtSituation(doubtSituation : List<String>) {
 
-    LazyColumn(
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
         modifier = Modifier
-            .fillMaxWidth()
+            .padding(
+                start = 10.dp,
+                end = 5.dp
+            )
     ) {
-//        item {
-//            Text(
-//                text = "의심되는 부분",
-//                fontSize = 25.sp,
-//                fontWeight = FontWeight.Bold,
-//                color = Color.Black,
-//                modifier = Modifier
-//            )
-//        }
-        items(doubtSituation){ situation ->
+        Text(
+            text = "🤔 ${stringResource(id = R.string.checkList_doubtSituationTitle)}",
+            fontSize = 23.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+        )
+
+        doubtSituation.forEachIndexed { index : Int, situation : String->
             Text(
-                text = situation,
-                fontWeight = FontWeight.SemiBold,
-                fontSize = 18.sp,
-                color = Color.Black
+                text = "${index + 1}. ${situation}",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(
+                        start = 10.dp,
+                        end = 5.dp,
+                        top = 3.dp
+                    )
             )
         }
     }
