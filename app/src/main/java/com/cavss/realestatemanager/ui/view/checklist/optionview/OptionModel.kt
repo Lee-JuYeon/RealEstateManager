6d58ad46cb413efbdfca28bbc9db25a2a6254b86
JuYeon Lee <cavss1118@gmail.com>
2021-12-04 22:04:04 +0900

    #8 OptionModel // 1. OptionModel을 seal calss 로 리팩토링

---
+++ app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/optionview/OptionModel.kt
@@ -0,0 +1,19 @@
+package com.cavss.realestatemanager.ui.view.checklist.optionview
+
+import com.cavss.realestatemanager.R
+
+sealed class OptionModel(
+    val type : String,
+    val inNative : Int
+) {
+    object BUY : OptionModel(type = "매매", inNative = R.string.checkList_option_buy)
+    object JEONSE : OptionModel(type = "전세", inNative = R.string.checkList_option_jeonse)
+    object MONTHLY_RENT : OptionModel(type = "월세", inNative = R.string.checkList_option_monthlyRent)
+    object SHORT_RENT : OptionModel(type = "단기임대", inNative = R.string.checkList_option_shortRent)
+
+    object OPTION_LIST {
+        val list = listOf(
+            BUY, JEONSE, MONTHLY_RENT, SHORT_RENT
+        )
+    }
+}
