2bb69c2695fabc9a462a103b8af1de012663407d
JuYeon Lee <cavss1118@gmail.com>
2021-12-05 22:55:18 +0900

    #15 ProgressCheckView // 1. 리스트 중 체크된 아이탬에 따라 반응형으로 progress animation설정

--- app/src/main/java/com/cavss/realestatemanager/ui/custom/progressbar/ProgressCheckView.kt
+++ app/src/main/java/com/cavss/realestatemanager/ui/custom/progressbar/ProgressCheckView.kt
@@ -1,6 +1,5 @@
-package com.cavss.realestatemanager.ui.view.progressbar
+package com.cavss.realestatemanager.ui.custom.progressbar
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
@@ -8,7 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
