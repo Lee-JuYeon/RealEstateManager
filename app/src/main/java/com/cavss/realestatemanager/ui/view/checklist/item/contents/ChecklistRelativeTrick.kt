ab21b3bc1564762cc0db1445ee7e4ee9b787e373
JuYeon Lee <cavss1118@gmail.com>
2021-12-05 23:16:24 +0900

    #19 ChecklistRelativeTrick //

--- app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/item/contents/ChecklistRelativeTrick.kt
+++ app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/item/contents/ChecklistRelativeTrick.kt
@@ -1,8 +1,38 @@
 package com.cavss.realestatemanager.ui.view.checklist.item.contents
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cavss.realestatemanager.R
 
 @Composable
-fun ChecklistRelativeTrick() {
-
+fun ChecklistRelativeTrick(relativeTricks : List<String>) {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .padding(
+                start = 30.dp,
+                end = 5.dp
+            )
+    ) {
+        relativeTricks.forEachIndexed { index : Int, situation : String->
+            Text(
+                text = "• $situation",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier
+            )
+        }
+    }
 }
