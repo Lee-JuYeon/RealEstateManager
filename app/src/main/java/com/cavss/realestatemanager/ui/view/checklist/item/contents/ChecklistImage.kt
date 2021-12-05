7f7444ec29d41c4e5ff27ba941ad206039242280
JuYeon Lee <cavss1118@gmail.com>
2021-12-05 23:19:39 +0900

    #23 ChecklistImage // 1. 클릭시 BottomSheet 보이기

---
+++ app/src/main/java/com/cavss/realestatemanager/ui/view/checklist/item/contents/ChecklistImage.kt
@@ -0,0 +1,58 @@
+package com.cavss.realestatemanager.ui.view.checklist.item.contents
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ChecklistImage(
+    images : List<String>
+) {
+    val showBottomSheet = remember { mutableStateOf(false) }
+    LazyRow(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+            .padding(
+                start = 30.dp,
+                end = 5.dp
+            )
+            .clickable {
+
+            }
+    ){
+        items(images){ image ->
+            Text(
+                text = image,
+                modifier = Modifier
+                    .padding(end = 5.dp)
+                    .size(
+                        85.dp, 100.dp
+                    )
+                    .clip(RoundedCornerShape(5.dp))
+                    .border(
+                        1.dp,
+                        Color.Black,
+                        RoundedCornerShape(5.dp)
+                    )
+                    .clickable {
+                        showBottomSheet.value = !showBottomSheet.value
+                    }
+            )
+        }
+    }
+}
