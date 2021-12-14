b778843670d132c0c3587c8dca8fe4da9c437d3e
JuYeon Lee <cavss1118@gmail.com>
2021-12-14 22:57:05 +0900

    #39 BottomSheetView // 1. StateValue를 변경하는 메소드가 필요함

--- app/src/main/java/com/cavss/realestatemanager/ui/custom/bottomsheet/BottomSheetView.kt
+++ app/src/main/java/com/cavss/realestatemanager/ui/custom/bottomsheet/BottomSheetView.kt
@@ -1,47 +1,101 @@
 package com.cavss.realestatemanager.ui.custom.bottomsheet
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import com.cavss.realestatemanager.ui.custom.popup.PopUpVM
 import com.cavss.realestatemanager.vm.MainVM
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@SuppressLint("CoroutineCreationDuringComposition")
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun BottomSheetView(
+    mainVM : MainVM
+) {
+    val bottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Expanded)
+    val bottomSheetScope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    val isShow = remember { mutableStateOf(false) }
+    mainVM.getBottomSheetShows.observe(LocalLifecycleOwner.current){
+        isShow.value = it
+
+        when(it){
+            true -> {
+                bottomSheetState.hide()
+            }
+            false -> {
+
+            }
+        }
+        Log.e("mException", "current value : ${bottomSheetState.currentValue}")
+    }
+
+    bottomSheetScope.launch {
+
+    }
+    if (isShow.value){
+        ModalBottomSheetLayout(
+            sheetBackgroundColor = Color.LightGray, // 시트 배경 색
+            sheetElevation = 5.dp,
+            sheetState = bottomSheetState,
+            sheetShape = RoundedCornerShape(
+                topStart = 30.dp,
+                topEnd = 30.dp
+            ),
+            scrimColor = Color.DarkGray.copy(0.5f), // 시트뷰 뒤의 배경색
+            sheetContent = {
+                BottomSheetItem()
+            }
+        ) {
+//            Text(
+//                text = "배경",
+//                color = Color.Magenta,
+//                fontSize = 20.sp,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .fillMaxHeight()
+//            )
+        }
+    }else{
+        Log.e("mException","show? : ${mainVM.getBottomSheetShows.value}")
+    }
+}
 
 @Composable
-fun BottomSheetView(mainVM : MainVM) {
-
-    val screenSize = LocalConfiguration.current
-    val screenWidth = screenSize.screenWidthDp.dp
-    val screenHeight = screenSize.screenHeightDp.dp
-
-    val showBottomSheet = remember { mutableStateOf(false) }
-
-    if (showBottomSheet.value){
-        Column(
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally,
+fun BottomSheetItem(){
+    Row(
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(500.dp)
+    ) {
+        Text(
+            text = "text11",
+            color = Color.Yellow,
+            fontSize = 20.sp,
             modifier = Modifier
-                .size(
-                    width = screenWidth,
-                    height = screenHeight
-                )
-                .background(Color.Yellow.copy(0.5f))
-                .border(
-                    1.dp,
-                    Color.Red,
-                    RectangleShape
-                )
-        ) {
-
-        }
+        )
     }
 }
