369585e4d687ec995448d235c61d8018c58fd93f
JuYeon Lee <cavss1118@gmail.com>
2021-12-17 23:24:02 +0900

    #41 ChecklistContentViewDoubtSituation // 1. File 이름 변경

--- app/src/main/java/com/cavss/realestatemanager/ui/view/bottomnaviscreen/checklist/item/contents/ChecklistContentViewDoubtSituation.kt
+++ app/src/main/java/com/cavss/realestatemanager/ui/view/bottomnaviscreen/checklist/item/contents/ChecklistContentViewDoubtSituation.kt
@@ -1,22 +1,17 @@
-package com.cavss.realestatemanager.ui.view.checklist.item.contents
+package com.cavss.realestatemanager.ui.view.bottomnaviscreen.checklist.item.contents
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.cavss.realestatemanager.R
 
 @Composable
-fun ChecklistDoubtSituation(doubtSituation : List<String>) {
+fun ChecklistContentViewDoubtSituation(doubtSituation : List<String>) {
 
     Column(
         verticalArrangement = Arrangement.Top,
