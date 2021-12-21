ab564c4d610638a86cac29ce6f0d9e8a2ed502a2
JuYeon Lee <cavss1118@gmail.com>
2021-12-21 23:34:49 +0900

    #58 TablayoutHeader // 1. 메모리 최적화 할 메소드가 있는지:? 2. refactoring 필요 3. 에러 잡기 위한 try- catch 이용 필요. 4. Pager 라이브러리를 Row, Column으로 대체

--- app/src/main/java/com/cavss/realestatemanager/ui/custom/tablayout/TabLayoutHeaader.kt
+++ app/src/main/java/com/cavss/realestatemanager/ui/custom/tablayout/TabLayoutHeaader.kt
@@ -1,77 +1,45 @@
 package com.cavss.realestatemanager.ui.custom.tablayout
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.material.TabRow
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import com.cavss.realestatemanager.R
-import com.cavss.realestatemanager.ui.custom.bottomnavi.BottomNaviItemView
-import com.cavss.realestatemanager.ui.custom.bottomnavi.BottomNaviModel
+import com.cavss.realestatemanager.model.TabLayoutModel
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun TabLayoutHeaader(
-    setNavController : NavHostController,
-    setTabLayoutModelList : List<TabLayoutModel>
+    setPagerState : PagerState,
+    setBackgroundColor : Color,
+    setIndicatorColor : Color,
+    setTabHeaderList : List<TabLayoutModel>,
+    setTabHeaderLayout : @Composable (Modifier, Boolean, TabLayoutModel) -> Unit
 ) {
-    LazyRow(
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth(),
-        content = {
-            items(setTabLayoutModelList){ model : TabLayoutModel ->
-                Text(
-                    text = stringResource(id = model.title),
-                    color = Color.White,
-                    fontWeight = if (setNavController.currentDestination?.route == model.route) {
-                        FontWeight.ExtraBold
-                    } else {
-                        FontWeight.Bold
-                    },
-                    fontSize = 20.sp,
-                    modifier = Modifier
-                        .padding(
-                            vertical = 5.dp
-                        )
-                        .clip(
-                            RoundedCornerShape(10.dp)
-                        )
-                        .background(colorResource(id = R.color.opacityGreen))
-                        .padding(
-                            start = 10.dp,
-                            end = 10.dp,
-                            top = 5.dp,
-                            bottom = 5.dp
-                        )
-                        .clickable {
-                            setNavController.navigate(model.route){
-                                popUpTo(setNavController.graph.findStartDestination().id)
-                                launchSingleTop = true
-                            }
+
+    val scope = rememberCoroutineScope()
+
+    TabRow(
+        selectedTabIndex = setPagerState.currentPage,
+        backgroundColor = setBackgroundColor,
+        contentColor = setIndicatorColor
+    ) {
+        setTabHeaderList.forEachIndexed { index, tabLayoutModel ->
+            val selectedTab = setPagerState.currentPage == index
+            setTabHeaderLayout(
+                Modifier
+                    .clickable {
+                        scope.launch(Dispatchers.IO) {
+                            setPagerState.scrollToPage(index)
                         }
-                )
-            }
+                    },
+                selectedTab,
+                tabLayoutModel
+            )
         }
-    )
+    }
 }
